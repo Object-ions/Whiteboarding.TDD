3275894c13d880768a2e93cd18e98443674c818f
object-ions <moshikoatia@gmail.com>
2023-11-11 21:08:47 -0800

    Refacor

--- src/js/reverseInt.js
+++ src/js/reverseInt.js
@@ -33,11 +33,19 @@
 
 // Solution 2:
 
+// export const reverseInt = function reverseInt(number) {
+//   const reversed = number.toString().split('').reverse().join('');
+  
+//   if (number < 0) {
+//     return parseInt(reversed) * -1;
+//   }
+//   return parseInt(reversed);
+// };
+
+// Refactoring:
+
 export const reverseInt = function reverseInt(number) {
   const reversed = number.toString().split('').reverse().join('');
   
-  if (number < 0) {
-    return parseInt(reversed) * -1;
-  }
-  return parseInt(reversed);
+  return parseInt(reversed) * Math.sign(number);
 };
