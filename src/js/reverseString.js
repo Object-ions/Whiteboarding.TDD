359c4febf7d5a944e4b1c918c12457804c076b7e
object-ions <moshikoatia@gmail.com>
2023-11-11 18:09:55 -0800

    Add 3rd soution. Pass all tests

--- src/js/reverseString.js
+++ src/js/reverseString.js
@@ -8,6 +8,7 @@
 //   return str.split('').reverse().join('');
 // };
 
+
 // *************** //
 
 // Solution 2:
@@ -15,12 +16,25 @@
 // 2. for each char in the string - take the char and add it to the start of 'reversed'
 // 3. Return the variable 'reversed'
 
-export const reverseString = (str) => {
-  let reversed = '';
+// export const reverseString = (str) => {
+//   let reversed = '';
   
-  for (let char of str) {
-    reversed = char + reversed;
-  }
+//   for (let char of str) {
+//     reversed = char + reversed;
+//   }
 
-  return reversed;
+//   return reversed;
+// };
+
+
+// *************** //
+
+// Solution 3:
+// 1. turn into an array, using .split()
+// 2. using .reduce() that take all the different values in the array and condence them all down into a single value
+
+export const reverseString = (str) => {
+  return str.split('').reduce((reversed, char) => {
+    return char + reversed;
+  }, '');
 };
