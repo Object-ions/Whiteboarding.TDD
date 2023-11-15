a03b5fce1f8cd7119066bdc3e1525d329c9ccd45
object-ions <moshikoatia@gmail.com>
2023-11-14 21:59:15 -0800

    Add 2nd solution and pass

--- src/js/arrayChunk.js
+++ src/js/arrayChunk.js
@@ -11,17 +11,29 @@
 
 // Solution 1
 
+// function arrayChunk(array, size) {
+//   const chunked = [];
+
+//   for (let element of array) {
+//     const last = chunked[chunked.length - 1];
+
+//     if (!last || last.length === size) {
+//       chunked.push([element]);
+//     } else {
+//       last.push(element);
+//     }
+//   }
+
+//   return chunked;
+// }
+
+
+// Solution 2:
 function arrayChunk(array, size) {
   const chunked = [];
 
-  for (let element of array) {
-    const last = chunked[chunked.length - 1];
-
-    if (!last || last.length === size) {
-      chunked.push([element]);
-    } else {
-      last.push(element);
-    }
+  for (let i = 0; i < array.length; i += size) {
+    chunked.push(array.slice(i, i + size));
   }
 
   return chunked;
