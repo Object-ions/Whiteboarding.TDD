81e19141faf00401f5d745d5389f22142248c4eb
object-ions <moshikoatia@gmail.com>
2023-11-14 21:57:07 -0800

    Update typos in test. Add solution and pass all tests.

--- src/js/arrayChunk.js
+++ src/js/arrayChunk.js
@@ -8,6 +8,23 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function arrayChunk(array, size) {}
 
-module.exports = chunk;
+// Solution 1
+
+function arrayChunk(array, size) {
+  const chunked = [];
+
+  for (let element of array) {
+    const last = chunked[chunked.length - 1];
+
+    if (!last || last.length === size) {
+      chunked.push([element]);
+    } else {
+      last.push(element);
+    }
+  }
+
+  return chunked;
+}
+
+module.exports = arrayChunk;
