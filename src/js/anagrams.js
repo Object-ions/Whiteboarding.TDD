6f8e804f2e9eb26f683133eaf2375661d189d921
object-ions <moshikoatia@gmail.com>
2023-11-17 22:22:19 -0800

    Add solution 1. pass 5 tests out of 6. WIP

--- src/js/anagrams.js
+++ src/js/anagrams.js
@@ -8,6 +8,35 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+// Solution :
+// 1. build a character map from string A and string B
+// 2. compare all the character inside the maps together
+
+function anagrams(stringA, stringB) {
+  const aCharMap = buildCharMap(stringA);
+  const bCharMap = buildCharMap(stringB);
+
+  if (Object.keys(aCharMap).length != Object.keys(bCharMap).length) {
+    return false;
+  }
+
+  for (let char in aCharMap) {
+    if (aCharMap[char] != bCharMap[char]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function buildCharMap(str) {
+  const charMap = {};
+  
+  for (let char in str.replace(/[^\w]/g).toLowerCase()) { //clean the string
+    charMap[char] = charMap[char] + 1 || 1;
+  }
+
+  return charMap;
+}
 
 module.exports = anagrams;
