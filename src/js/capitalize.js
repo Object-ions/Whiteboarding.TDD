4503df67b10cbb969cfa94a41fbb598ef18d9d5a
object-ions <moshikoatia@gmail.com>
2023-11-19 00:02:52 -0800

    Add solution 2 and pass all tests

--- src/js/capitalize.js
+++ src/js/capitalize.js
@@ -8,6 +8,10 @@
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
 
+// ********************** //
+// ***** Solution 1 ***** //
+// ********************** //
+
 // Make an empty array of 'words'
 // split the input by spaces
 // for each word in the array:
@@ -16,15 +20,38 @@
 //    push result into words array
 //  join words into a string and return
 
+// function capitalize(str) {
+//   const words = [];
+
+//   for (let word of str.split(' ')) {
+//     words.push(word[0].toUpperCase() + word.slice(1));
+//   }
+
+//   return words.join(' ');
+// }
+
+
+// ********************** //
+// ***** Solution 2 ***** //
+// ********************** //
+
+// 1. Create 'result' whcich is the first char of the input is capitalize
+// 2. For each char in the string
+//      if the char to the left a space
+//      capitalize and add to result
+//      else add it to result
 
 function capitalize(str) {
-  const words = [];
+  let result = str[0].toUpperCase();
 
-  for (let word of str.split(' ')) {
-    words.push(word[0].toUpperCase() + word.slice(1));
+  for (let i = 1; i < str.length; i++) {
+    if (str[i -1] === ' ') {
+      result += str[i].toUpperCase();
+    } else {
+      result += str[i]
+    }
   }
-
-  return words.join(' ');
+  return result;
 }
 
 module.exports = capitalize;
